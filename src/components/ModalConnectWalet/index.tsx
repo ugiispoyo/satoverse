31f102b2a745f4d045e20b08797662265159fdac
Ugi <ugi.widodo@kitabisa.com>
2025-05-19 23:54:05 +0700

    feat: update modal connect wallet responsive

--- src/components/ModalConnectWalet/index.tsx
+++ src/components/ModalConnectWalet/index.tsx
@@ -22,69 +22,86 @@ const ModalConnectWalet: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-[999] flex items-center justify-center bg-[#000000bd] h-screen w-full">
-      <div className="bg-[url('/images/bg-modal-connect-wallet.svg')] bg-no-repeat bg-size-[100%] rounded-2xl w-full max-w-[480px] min-h-[580px] transition-all transform">
-        <div className="flex items-center justify-center flex-col py-10 px-8 relative">
-          <button className="absolute top-10 right-13 cursor-pointer" onClick={onClose}>
-            <img
-              src="/icons/close.svg"
-              alt="close"
-              className="w-[20px] h-[20px]"
-            />
-          </button>
+    <div className="fixed inset-0 z-[999] flex items-center justify-center bg-[#000000bd] h-screen w-full px-4">
+      <div className="bg-[url('/images/bg-modal-connect-wallet.svg')] relative bg-no-repeat bg-cover bg-center rounded-2xl w-full max-w-[480px] min-h-[580px] sm:min-h-[580px] min-h-[90vh] transition-all transform px-4 sm:px-8">
+        {/* CLOSE BUTTON */}
+        <button
+          className="absolute top-10 right-13 max-[460px]:right-5 cursor-pointer z-[999]"
+          onClick={onClose}
+        >
+          <img
+            src="/icons/close.svg"
+            alt="close"
+            className="w-[20px] h-[20px]"
+          />
+        </button>
+        <div className="flex items-center justify-center flex-col py-10 relative">
+          {/* WALLET ICON */}
           <img
             src="/images/wallet.svg"
             alt="wallet"
-            className="w-[40px] h-[40px]"
+            className="w-[40px] h-[40px] sm:w-[45px] sm:h-[45px]"
           />
-          <h2 className="font-joystix text-[24px] text-gradient-8">
+
+          {/* TITLE */}
+          <h2 className="font-joystix text-[22px] sm:text-[24px] text-gradient-8">
             Connect Wallet
           </h2>
-          <p className="text-[10px] uppercase text-[#E3DAAC] font-extrabold font-orbitron text-center w-full max-w-[370px] mt-1 px-5">
+
+          {/* DESCRIPTION */}
+          <p className="text-[10px] sm:text-[11px] uppercase text-[#E3DAAC] font-extrabold font-orbitron text-center w-full max-w-[370px] mt-1 px-2 sm:px-5">
             If you already have a wallet, select your desired wallet from the
             options below. If you don't have a wallet, download{" "}
             <span className="underline">Best Wallet</span> to get started.
           </p>
-          <div className="w-full flex gap-4 justify-center items-center flex-col mt-4 px-4">
-            <button className="bg-btn-best-wallet font-orbitron font-[900] w-full flex justify-between items-center">
-              <span className="text-gradient-5 text-base ">BEST WALLET</span>
+
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4 justify-center items-center flex-col mt-4">
+            {/* BEST WALLET */}
+            <button className="bg-btn-best-wallet font-orbitron font-[900] w-full flex justify-between items-center h-[50px] px-4 sm:h-[56px]">
+              <span className="text-gradient-5 text-sm sm:text-base">
+                BEST WALLET
+              </span>
               <img
                 src="/icons/best-wallet.svg"
                 alt="best-wallet"
-                className="w-full max-w-[50px] h-auto"
+                className="max-w-[35px] sm:max-w-[50px]"
               />
             </button>
-            <button className="bg-btn-other-connect-wallet text-gradient-5 text-base font-orbitron font-[900] w-full flex justify-between items-center">
-              <span className="text-gradient-5 text-base">WALLET CONNECT</span>
-              <img
-                src="/icons/tether.svg"
-                alt="tether"
-                className="w-full max-w-[50px] h-auto"
-              />
-            </button>
-            <button className="bg-btn-other-connect-wallet text-gradient-5 text-base font-orbitron font-[900] w-full flex justify-between items-center">
-              <span className="text-gradient-5 text-base">METAMASK</span>
-              <img
-                src="/icons/metamask.svg"
-                alt="metamask"
-                className="w-full max-w-[50px] h-auto"
-              />
-            </button>
-            <button className="bg-btn-other-connect-wallet text-gradient-5 text-base font-orbitron font-[900] w-full flex justify-between items-center">
-              <span className="text-gradient-5 text-base">COINBASE WALLET</span>
-              <img
-                src="/icons/coinbase.svg"
-                alt="coinbase-wallet"
-                className="w-full max-w-[50px] h-auto"
-              />
-            </button>
+
+            {/* OTHER WALLETS */}
+            {[
+              { name: "WALLET CONNECT", icon: "/icons/tether.svg" },
+              { name: "METAMASK", icon: "/icons/metamask.svg" },
+              { name: "COINBASE WALLET", icon: "/icons/coinbase.svg" },
+            ].map((wallet, index) => (
+              <button
+                key={index}
+                className="bg-btn-other-connect-wallet text-gradient-5 font-orbitron font-[900] w-full flex justify-between items-center h-[50px] px-4 sm:h-[56px]"
+              >
+                <span className="text-gradient-5 text-sm sm:text-base">
+                  {wallet.name}
+                </span>
+                <img
+                  src={wallet.icon}
+                  alt={wallet.name}
+                  className="max-w-[35px] sm:max-w-[50px]"
+                />
+              </button>
+            ))}
           </div>
+
+          {/* NO WALLET */}
           <Link
             href="#"
             className="w-full flex justify-center items-center mt-4"
           >
-            <img src="/icons/wallet-remove.svg" alt="wallet-remove" />
-            <span className="text-[#E3DAAC] font-orbitron font-[800] ml-2 text-[10px]">
+            <img
+              src="/icons/wallet-remove.svg"
+              alt="wallet-remove"
+              className="w-[14px] h-[14px] sm:w-[16px] sm:h-[16px]"
+            />
+            <span className="text-[#E3DAAC] font-orbitron font-[800] ml-2 text-[10px] sm:text-[11px]">
               I DON'T HAVE A WALLET
             </span>
           </Link>
