2aef0687600c19fe3d56e0ff379bd4b65fd0d6f7
Ugi <ugi.widodo@kitabisa.com>
2025-05-21 10:24:13 +0700

    feat: add status pending & update status loading

--- src/components/ModalStatus/index.tsx
+++ src/components/ModalStatus/index.tsx
@@ -5,7 +5,7 @@ import React, { useEffect } from "react";
 type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
-  status: "success" | "error" | "pending";
+  status: "success" | "error" | "pending" | "loading";
 };
 
 const ModalStatus: React.FC<ModalProps> = ({ isOpen, onClose, status }) => {
@@ -23,17 +23,61 @@ const ModalStatus: React.FC<ModalProps> = ({ isOpen, onClose, status }) => {
 
   if (!isOpen) return null;
 
+  const renderStatus = () => {
+    switch (status) {
+      case "success":
+        return (
+          <>
+            <img
+              src="/icons/wallet-checked.svg"
+              alt="wallet"
+              className="w-[40px] h-[40px] sm:w-[45px] sm:h-[45px] mb-2"
+            />
+            <span className="block uppercase w-full text-center text-[20px] text-gradient-7 font-joystix font-[900]">
+              transaction Submitted
+            </span>
+          </>
+        );
+      case "error":
+        return (
+          <>
+            <img
+              src="/icons/wallet-remove.svg"
+              alt="wallet"
+              className="w-[40px] h-[40px] sm:w-[45px] sm:h-[45px] mb-2"
+            />
+            <span className="block w-full uppercase text-center text-[20px] text-gradient-3 font-joystix font-[900]">
+              transaction failed
+            </span>
+          </>
+        );
+      case "pending":
+        return (
+          <>
+            <img
+              src="/icons/wallet-pending.svg"
+              alt="wallet"
+              className="w-[40px] h-[40px] sm:w-[45px] sm:h-[45px] mb-2"
+            />
+            <span className="block uppercase w-full text-center text-[20px] text-[#E3DAAC] font-joystix font-[900]">
+              transaction pending
+            </span>
+          </>
+        );
+    }
+  };
+
   return (
     <div className="fixed inset-0 z-[999] flex items-center justify-center bg-[#000000bd] h-screen w-full">
       <div
         className={clsx(
           "relative rounded-2xl w-full max-w-[480px] min-h-[293px] transition-all transform max-sm:px-4",
-          status === "pending"
+          status === "loading"
             ? "justify-center items-center flex"
             : "bg-[url('/images/bg-modal-connect-wallet.svg')] bg-no-repeat bg-cover bg-center flex justify-center"
         )}
       >
-        {status === "pending" ? (
+        {status === "loading" ? (
           <>
             <div className="w-16 h-16 border-4 border-t-transparent border-[#b3f487] rounded-full animate-spin"></div>
           </>
@@ -53,32 +97,8 @@ const ModalStatus: React.FC<ModalProps> = ({ isOpen, onClose, status }) => {
 
             <div className="flex items-center justify-center flex-col md:py-8 py-4 relative max-sm:px-5">
               {/* WALLET ICON */}
-              {status === "success" ? (
-                <>
-                  <img
-                    src="/icons/wallet-checked.svg"
-                    alt="wallet"
-                    className="w-[40px] h-[40px] sm:w-[45px] sm:h-[45px] mb-2"
-                  />
-                  <span className="block uppercase w-full text-center text-[20px] text-gradient-7 font-joystix font-[900]">
-                    transaction Submitted
-                  </span>
-                </>
-              ) : (
-                status === "error" && (
-                  <>
-                    <img
-                      src="/icons/wallet-remove.svg"
-                      alt="wallet"
-                      className="w-[40px] h-[40px] sm:w-[45px] sm:h-[45px] mb-2"
-                    />
-                    <span className="block w-full uppercase text-center text-[20px] text-gradient-3 font-joystix font-[900]">
-                      transaction failed
-                    </span>
-                  </>
-                )
-              )}
-              {}
+              {renderStatus()}
+
               <Link href="#">
                 <span className="block text-center font-azeret_mono text-[12px] mb-4 text-[#E3DAAC]">
                   View on explorer
